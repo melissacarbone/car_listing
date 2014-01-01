39df833bba90bc000556bc09cf076106883afacb
Melissa Carbone <mcarbone1@gmail.com>
2014-01-01 14:23:43 -0500

    initial commit, add rspec and fix database credentials

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarListing::Application.config.secret_key_base = 'f956590093e83380dea5f47bbf068f167a0fccd40827051c61c9aebf69122487fa7f6cfe618d15e1f8d7240e1b1d871ae947a18fbcb4f8f587609680a2e97ccf'
